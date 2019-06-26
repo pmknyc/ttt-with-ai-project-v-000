ae601b053992e7619d7ae92f8f0b140102cf1fcf
pmknyc <pmknyc@optimum.net>
2019-06-26 12:59:04 -0400

    20190626 board.rb cleanup code, add comments

--- lib/board.rb
+++ lib/board.rb
@@ -11,25 +11,29 @@ class Board
     reset!
   end
 
-# reset! method can reset the state of the cells to a pre-game empty board
+# reset! method resets state of cells to a pre-game empty board
   def reset!
     @cells = Array.new(9," ")
   end
 
-# input_index: convert user input to array index
-  def input_index(input)
-      input_integer(input) - 1
-  end
-
-  # input_integer: helper method
-  def input_integer(input)
-    input.to_i #user string input to integer
-              # so that #input_index can convert it to array index
-  end
-
-# #position takes user input string from "1" to "9"
-#   convert to integer & board.cells array index,
+# position:  helper method for #taken?
 #   look up the board cell value at that array index
+#   takes user's #move input string from "1" to "9"
+#   convert to integer & board.cells array index (w/ helper methods)
+# RETURN  player.token value "X" or "O"
+  def position(input)
+    @cells[input_index(input)]
+  end
+
+  # input_index: helper to #position
+  def input_index(input)
+    input_integer(input) - 1 #convert player #move input to array index
+  end
+
+  # input_integer: helper to #input_index
+  def input_integer(input)
+    input.to_i  # user string input to integer
+  end           # so that #input_index converts to array index
 
 # taken?
 # if player's chosen move position is occupied: true, false if not
@@ -37,11 +41,6 @@ class Board
     position(input) != " "
   end
 
-# position - helper method for #taken?
-  # RETURN  player.token value "X" or "O"
-  def position(input)
-    @cells[input_index(input)]
-  end
 
 # valid_move? 
     #check that moves are between 1-9 and not taken; Boolean return
@@ -49,8 +48,10 @@ class Board
     input_integer(input).between?(1,9) && !taken?(input)
   end
 
-# #update represents updating board when player makes a move.
-# #update has two arguments,
+# #update adds player moves to board object
+#   called by game.turn to update after every play in game
+#   receives 2 args:  player.move_input (1 to 9 as string)
+                      current_player
 # first is position the user wants to occupy in the form of 1-9 strings
 # that you will need to convert to the board cells' array index,
 # along with the player object making the move.
@@ -61,8 +62,9 @@ class Board
     @cells[input.to_i - 1] = player.token
   end
 
-# #turn_count returns how many positions in cells array are filled
-#  filled cell count shows num of turns already played
+# turn_count tracks number of turns played in game
+#    up to now
+# RETURN is count of filled board.cells AKA turn_count
   def turn_count
     count = 0
     self.cells.each do |cell|
@@ -71,13 +73,15 @@ class Board
     count
   end
 
-# #full? returns truthy when board cells entirely
-# occupied with "X" and "O" token strings
+# #full? returns truthy when all board cells
+# occupied with "X" or "O" token strings
   def full?
     self.cells.all?{|cell| cell == "X" || cell == "O"}
   end
 
-# #display:  board can print current state
+# #display: board prints current occupied state of 
+#   all cells as empty, "X" or "O"
+#   this method called at end of every #turn
   def display
     c = self.cells
     puts " #{c[0]} | #{c[1]} | #{c[2]} "
